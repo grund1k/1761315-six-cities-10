955df7acb57ca34f216b9e0cf52bfff54e39aa3f
grund1k <dfyz8607@gmail.com>
2022-07-31 19:35:01 +0300

    Добавляет карту

---
+++ project/src/components/map/map.tsx
@@ -0,0 +1,43 @@
+import {Icon, Marker} from 'leaflet';
+import { useRef, useEffect } from 'react';
+import {City, Offers} from '../../types/offer';
+import useMap from './../../hooks/useMap/useMap';
+import {URL_MARKER_DEFAULT} from '../../const';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  city: City;
+  offers: Offers;
+}
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39],
+});
+
+const Map = ({city, offers} : MapProps) : JSX.Element => {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        const marker = new Marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude
+        });
+
+        marker.setIcon(defaultCustomIcon).addTo(map);
+      });
+    }
+  }, [map, offers]);
+
+  return(
+    <section className="cities__map map" ref={mapRef}>
+
+    </section>
+  );
+};
+
+export default Map;
